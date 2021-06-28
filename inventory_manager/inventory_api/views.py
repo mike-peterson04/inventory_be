40bc211ad48e5cd920a4bb621bcaa332db9227fe
Mike Peterson <mpeterson2004@gmail.com>
2021-06-27 22:35:24 -0600

    minor formatting changes

--- inventory_manager/inventory_api/views.py
+++ inventory_manager/inventory_api/views.py
@@ -24,6 +24,7 @@ class EmployeeActions(APIView):
         return Response(ProductSerializer(product_return, many=True).data, status=status.HTTP_200_OK)
 
 
+
 class EmployeeUnprotected(APIView):
     def post(self, request):
         serializer = EmployeeSerializer(data=request.data)
@@ -121,8 +122,6 @@ class ProductHandler(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         try:
@@ -177,10 +176,9 @@ class StatusHandler(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-# def validate(user):
-#     try:
-#         user = Employees.objects.get(user=user)
-#         return user
-#     except:
-#         return "Access not yet validated please check with your system administrator on request status"
+def status_key():
+    possible_status = Status.objects.all()
+    result = {}
+    for state in possible_status:
+        result[state.id]=state.name
+    return result
